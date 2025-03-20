9f3ca7180ba48d725ed02108e08962f5e5731e9f
kamil3397 <kkgolkiper33@gmail.com>
2025-03-20 08:56:51 +0100

    pr do sprawdzenia

---
+++ src/pair-programming/TodoList.tsx
@@ -0,0 +1,49 @@
+import { Todo } from "./TodoApp"
+
+interface TodoListProps{
+    todos: Todo[]
+    toggleTodo: (id: number) => void
+    deleteTodo: (id: number)=> void
+}
+export const TodoList: React.FC<TodoListProps> = ({todos, toggleTodo, deleteTodo})=>{
+ if (todos.length === 0) return <p>No todos yet</p>
+
+    return(
+        <>
+        {todos.length >= 0 ? <h5>Click checkbox if u completed the task</h5> : null }
+        
+        <ul>
+            {todos.map(({id, text, completed})=>(
+                <li key={id} style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    padding: "10px 0",
+                    borderBottom: "1px solid #ddd",}}>
+
+                <span style={{ textDecoration: completed ? "line-through" : "none" }}>
+                {text}
+                 </span>
+
+                    <input
+                    type="checkbox"
+                    checked={completed}
+                    onChange={()=> toggleTodo(id)}
+                    style={{
+                        transform: "scale(1.3)",
+                        marginRight: "10px",}}
+                    />
+                    <button onClick={()=> deleteTodo(id)} 
+                    style={{ 
+                    padding: "5px 10px",
+                    background: "#dc3545",
+                    border: "none",
+                    borderRadius: "5px",
+                    color: "#fff",
+                    cursor: "pointer",}}>Delete</button>
+                </li>
+            ))}
+        </ul>
+        </>
+ )
+}
